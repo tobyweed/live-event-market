7e81dd8bbb621246f0ce8c78618c64842f44986e
Toby Weed <tobyweed@Tobys-MacBook-Pro.local>
2018-05-07 16:51:11 -0400

    tried to get react working right

--- static/js/components/App.jsx
+++ static/js/components/App.jsx
@@ -1,35 +1,26 @@
-import React from "react";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-export default class App extends React.Component {
-  render () {
-    return <p> Hello React!</p>;
-  }
+const Title = () => {
+  return (
+      <div className="title">
+        <h1>Title</h1>
+        <Link to="/list"><button>Show the List</button></Link>
+      </div>
+  )
 }
-
-
-// import React, { Component } from 'react';
-// import { Link } from 'react-router-dom';
-//
-// const Title = () => {
-//   return (
-//       <div className="title">
-//         <h1>Title</h1>
-//         <Link to="/list"><button>Show the List</button></Link>
-//       </div>
-//   )
-// }
-// const List = () => {
-//   return (
-//     <div className="nav">
-//       <ul>
-//         <li>list item</li>
-//         <li>list item</li>
-//       </ul>
-//       <Link to="/"><button>Back Home</button></Link>
-//     </div>
-//   )
-// }
-// module.exports = {
-//   Title,
-//   List
-// };
+const List = () => {
+  return (
+    <div className="nav">
+      <ul>
+        <li>list item</li>
+        <li>list item</li>
+      </ul>
+      <Link to="/"><button>Back Home</button></Link>
+    </div>
+  )
+}
+module.exports = {
+  Title,
+  List
+};
--- static/js/index.jsx
+++ static/js/index.jsx
@@ -1,26 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Title, List } from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("content"));
-
-
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router, Route} from 'react-router-dom';
-// import { Title, List } from './components/App';
-//
-// ReactDOM.render(
-//   <Router>
-//     <div>
-//     <p>asdfs</p>
-//     <Router exact path='/' component={Title} />
-//     <Router path='/list' component={List} />
-//     </div>
-//   </Router>,
-//   document.getElementById('content')
-// );
+ReactDOM.render(
+  <Router>
+    <div>
+    <Router exact path='/' component={Title} />
+    <Router path='/list' component={List} />
+    </div>
+  </Router>,
+  document.getElementById('content')
+);
