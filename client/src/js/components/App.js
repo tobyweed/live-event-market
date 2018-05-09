7df11edfd6e036a3ea6f9e0a651fa9247e074632
Toby Weed <tobyweed@Tobys-MacBook-Pro.local>
2018-05-09 12:25:14 -0400

    implement react-router

---
+++ client/src/js/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../../css/App.css';
+
+class App extends Component {
+	render() {
+		return (
+			<div>
+				<div className="App">APP</div>
+			</div>
+		);
+	}
+}
+
+export default App;
