555a070837ff4d868bff1ec6a019466cbb66b201
Russell Boswell <Bloodyboswell@gmail.com>
2018-05-22 22:23:18 -0400

    somthing usefull

--- client/src/index.js
+++ client/src/index.js
@@ -8,23 +8,26 @@ import Nav from './js/components/Nav/Nav';
 import registerServiceWorker from './registerServiceWorker';
 
 //import master components
-import _home from 'components/masterComponents/home';
-import _search from 'components/masterComponents/search';
-import _event from 'components/masterComponents/event';
-import _user from 'components/masterComponents/user';
-import _promoter from 'components/masterComponents/promoter';
+import Home from './js/components/masterComponents/Home.js';
+//import _search from './components/masterComponents/search';
+//import _event from './components/masterComponents/event';
+//import _user from './components/masterComponents/user';
+//import _promoter from './components/masterComponents/promoter';
+
 
 ReactDOM.render(
 	<Router>
 		<div>
-			<Route path="/" component={_home} />
-	        <Route path="/Search" component={_search} />
-	        <Route path="/Event" component={_event} />
-	        <Route path="/User" component={_user} />
-	        <Route path="/Pomoter" component={_promoter} />
+			<Route path="/" component={Home} />
+
 		</div>
 	</Router>,
 
 	document.getElementById('root')
 );
 registerServiceWorker();
+
+//<Route path="/Search" component={_search} />
+//<Route path="/Event" component={_event} />
+//<Route path="/User" component={_user} />
+//<Route path="/Pomoter" component={_promoter} />
--- client/src/js/components/masterComponents/home.js
+++ client/src/js/components/masterComponents/home.js
@@ -1,24 +1,22 @@
-import React from 'react';
-
-
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 //import master components
-import  from 'components/masterComponents/home';
-import _search from 'components/masterComponents/search';
-import _event from 'components/masterComponents/event';
-import _user from 'components/masterComponents/user';
-import _promoter from 'components/masterComponents/promoter';
+import  Nav from '../childComponents/Nav.js';
+import  FrontPage from '../childComponents/FrontPage.js';
+import  About from '../childComponents/About.js';
+import  Footer from '../childComponents/Footer.js';
 
-ReactDOM.render(
-	<Router>
+class Home extends Component{
+render() {
+	return (
 		<div>
-			<Route path="/" component={_home} />
-	        <Route path="/Search" component={_search} />
-	        <Route path="/Event" component={_event} />
-	        <Route path="/User" component={_user} />
-	        <Route path="/Pomoter" component={_promoter} />
+		<Nav></Nav>
+		<FrontPage></FrontPage>
+		<About></About>
+		<Footer></Footer>
 		</div>
-	</Router>,
+	);}
+}
+export default Home;
